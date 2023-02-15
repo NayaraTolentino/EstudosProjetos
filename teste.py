7135f6e30e3d4850c2ccaaf6a5864a379d9dac24
Nayara Tolentino <nayara.tolentino@linearsm.com.br>
2023-02-15 14:04:23 -0300

    Django

---
+++ teste.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+print("Hello World")
+a = 2 + 5
+
+
+def soma(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    return a + b
